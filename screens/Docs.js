08ead3f0c27511ddc1ec7914ef03a673ed5d509f
SONKENG MBOGNING Brice <cruzorbladex@gmail.com>
2022-07-25 17:54:13 +0100

    Firebase has been added to the app, with auth, firestore and storage

--- screens/Docs.js
+++ screens/Docs.js
@@ -1,7 +1,22 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {View, Text, StyleSheet} from 'react-native';
+import firestore from '@react-native-firebase/firestore';
 
-const Docs = () => {
+const Docs = ({navigation, route}) => {
+    const getDocs = async () => {
+        const documents = await firestore()
+        .collection('resources')
+        .doc('documents')
+        .collection('validated')
+        .get()
+
+        documents.forEach(document => {
+            console.log(document.data())
+        })
+    }
+    useEffect(() => {
+        getDocs();
+    }, [])
     return (
         <View style={styles.container}>
             <Text>Docs Screen</Text>
