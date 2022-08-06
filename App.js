fc4059806fa88d9436ca256c86e7c2070209896e
SONKENG MBOGNING Brice <cruzorbladex@gmail.com>
2022-08-06 14:02:42 +0100

    Added a splash screen to the app

--- App.js
+++ App.js
@@ -1,9 +1,13 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import AuthContextProvider from './context/AuthContext';
 import { NavigationContainer } from '@react-navigation/native';
 import Routes from './navigation/Routes';
+import RNBootSplash from "react-native-bootsplash";
 
 const App = () => {
+  useEffect(() => {
+    RNBootSplash.hide({fade:true})
+  }, []);
   return (
     <AuthContextProvider>
       <NavigationContainer>
